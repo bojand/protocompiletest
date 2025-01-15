67c9eae7adf7843eee5c6afba5b4aaff648cf174
Bojan <dbojan@gmail.com>
2025-01-14 22:31:12 -0400

    initial test

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
 # protocompiletest
+
+## Run
+
+Repeat
+
+```sh
+$ go test -v
+```
+
+Followed by cache clean
+
+```sh
+$ go clean -cache -testcache
+```
+
+### Example
+
+```sh
+❯ go test -v
+=== RUN   TestPlainProto
+!!! ACTUAL JSON DATA:
+{"version":1, "id":"444", "createdAt":"2023-07-15T10:00:00Z", "lastUpdatedAt":"2023-07-15T11:00:00Z", "orderValue":0, "lineItems":[], "revision":0}
+!!! EXPECTED JSON DATA:
+{"version":1, "id":"444", "createdAt":"2023-07-15T10:00:00Z", "lastUpdatedAt":"2023-07-15T11:00:00Z", "orderValue":0, "lineItems":[], "revision":0}
+--- PASS: TestPlainProto (0.00s)
+PASS
+ok  	github.com/bojand/protocompiletest	0.266s
+
+❯ go clean -cache -testcache
+
+❯ go test -v
+=== RUN   TestPlainProto
+!!! ACTUAL JSON DATA:
+{"version":1, "id":"444", "createdAt":"2023-07-15T10:00:00Z", "lastUpdatedAt":"2023-07-15T11:00:00Z", "orderValue":0, "lineItems":[], "revision":0}
+!!! EXPECTED JSON DATA:
+{"version":1, "id":"444", "createdAt":"2023-07-15T10:00:00Z", "lastUpdatedAt":"2023-07-15T11:00:00Z", "orderValue":0, "lineItems":[], "revision":0}
+--- PASS: TestPlainProto (0.00s)
+PASS
+ok  	github.com/bojand/protocompiletest	0.270s
+
+❯ go clean -cache -testcache
+
+❯ go test -v
+=== RUN   TestPlainProto
+!!! ACTUAL JSON DATA:
+{"version":1, "id":"444", "createdAt":"2023-07-15T10:00:00Z", "lastUpdatedAt":"2023-07-15T11:00:00Z", "orderValue":0, "lineItems":[], "revision":0}
+    main_test.go:111:
+        	Error Trace:	/Users/bojandjurkovic/dev/protocompiletest/main_test.go:111
+        	Error:      	Not equal:
+        	            	expected: []byte{0x8, 0x1, 0x12, 0x3, 0x34, 0x34, 0x34, 0x1a, 0x6, 0x8, 0xa0, 0xdc, 0xc9, 0xa5, 0x6, 0x22, 0x6, 0x8, 0xb0, 0xf8, 0xc9, 0xa5, 0x6}
+        	            	actual  : []byte{0x22, 0x6, 0x8, 0xb0, 0xf8, 0xc9, 0xa5, 0x6, 0x8, 0x1, 0x12, 0x3, 0x34, 0x34, 0x34, 0x1a, 0x6, 0x8, 0xa0, 0xdc, 0xc9, 0xa5, 0x6}
+
+        	            	Diff:
+        	            	--- Expected
+        	            	+++ Actual
+        	            	@@ -1,4 +1,4 @@
+        	            	 ([]uint8) (len=23) {
+        	            	- 00000000  08 01 12 03 34 34 34 1a  06 08 a0 dc c9 a5 06 22  |....444........"|
+        	            	- 00000010  06 08 b0 f8 c9 a5 06                              |.......|
+        	            	+ 00000000  22 06 08 b0 f8 c9 a5 06  08 01 12 03 34 34 34 1a  |"...........444.|
+        	            	+ 00000010  06 08 a0 dc c9 a5 06                              |.......|
+        	            	 }
+        	Test:       	TestPlainProto
+!!! EXPECTED JSON DATA:
+{"version":1, "id":"444", "createdAt":"2023-07-15T10:00:00Z", "lastUpdatedAt":"2023-07-15T11:00:00Z", "orderValue":0, "lineItems":[], "revision":0}
+--- FAIL: TestPlainProto (0.00s)
+FAIL
+exit status 1
+FAIL	github.com/bojand/protocompiletest	0.272s
+```
+
+## Proto
+
+```sh
+buf generate --template=testdata/proto/buf.gen.yaml testdata/proto
+```
---
+++ main_test.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/bufbuild/protocompile"
+	"github.com/bufbuild/protocompile/reporter"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoregistry"
+	"google.golang.org/protobuf/types/dynamicpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	shopv2 "github.com/bojand/protocompiletest/testdata/proto/gen/shop/v2"
+)
+
+func TestPlainProto(t *testing.T) {
+
+	ctx := context.Background()
+
+	filePaths := []string{
+		"shop/v2/order.proto",
+	}
+
+	logger := zaptest.NewLogger(t)
+
+	errorReporter := func(err reporter.ErrorWithPos) error {
+		position := err.GetPosition()
+		logger.Warn("failed to parse proto file to descriptor",
+			zap.String("file", position.Filename),
+			zap.Int("line", position.Line),
+			zap.Error(err))
+		return nil
+	}
+
+	compiler := protocompile.Compiler{
+		Resolver: protocompile.WithStandardImports(&protocompile.SourceResolver{
+			ImportPaths: []string{"./testdata/proto"},
+		}),
+		Reporter: reporter.NewReporter(errorReporter, nil),
+	}
+
+	compiledFiles, err := compiler.Compile(ctx, filePaths...)
+	require.NoError(t, err)
+	assert.NotEmpty(t, compiledFiles)
+
+	registry := new(protoregistry.Types)
+	for _, descriptor := range compiledFiles {
+		mds := descriptor.Messages()
+
+		for i := 0; i < mds.Len(); i++ {
+			mt := dynamicpb.NewMessageType(mds.Get(i))
+			registry.RegisterMessage(mt)
+		}
+	}
+
+	// expected
+
+	// inputData := `{"version":1,"id":"444","createdAt":"2023-07-15T10:00:00Z","lastUpdatedAt":"2023-07-15T11:00:00Z","deliveredAt":"2023-07-15T12:00:00Z","completedAt":"2023-07-15T13:00:00Z"}`
+	inputData := `{"version":1,"id":"444","createdAt":"2023-07-15T10:00:00Z","lastUpdatedAt":"2023-07-15T11:00:00Z"}`
+
+	messageType, err := registry.FindMessageByURL("shop.v2.Order")
+	require.NoError(t, err)
+	require.NotEmpty(t, messageType)
+
+	md := messageType.Descriptor()
+	actualMsg := dynamicpb.NewMessage(md)
+	unmarshaller := protojson.UnmarshalOptions{
+		DiscardUnknown: false,
+		Resolver:       registry,
+	}
+	err = unmarshaller.Unmarshal([]byte(inputData), actualMsg)
+	assert.NoError(t, err)
+
+	actualData, err := proto.Marshal(actualMsg)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, actualData)
+
+	jsonData, err := marshalJSON(actualMsg, registry)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, jsonData)
+	fmt.Println("!!! ACTUAL JSON DATA:")
+	fmt.Println(string(jsonData))
+
+	// actual
+
+	orderCreatedAt := time.Date(2023, time.July, 15, 10, 0, 0, 0, time.UTC)
+	orderUpdatedAt := time.Date(2023, time.July, 15, 11, 0, 0, 0, time.UTC)
+	// orderDeliveredAt := time.Date(2023, time.July, 15, 12, 0, 0, 0, time.UTC)
+	// orderCompletedAt := time.Date(2023, time.July, 15, 13, 0, 0, 0, time.UTC)
+
+	expectedMsg := shopv2.Order{
+		Version:       1,
+		Id:            "444",
+		CreatedAt:     timestamppb.New(orderCreatedAt),
+		LastUpdatedAt: timestamppb.New(orderUpdatedAt),
+		// DeliveredAt:   timestamppb.New(orderDeliveredAt),
+		// CompletedAt:   timestamppb.New(orderCompletedAt),
+	}
+
+	expectData, err := proto.Marshal(&expectedMsg)
+	require.NoError(t, err)
+
+	assert.Equal(t, expectData, actualData)
+
+	jsonData, err = marshalJSON(&expectedMsg, registry)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, jsonData)
+	fmt.Println("!!! EXPECTED JSON DATA:")
+	fmt.Println(string(jsonData))
+}
+
+func marshalJSON(m proto.Message, registry *protoregistry.Types) ([]byte, error) {
+	marshaller := protojson.MarshalOptions{
+		EmitDefaultValues: true,
+		Resolver:          registry,
+	}
+
+	return marshaller.Marshal(m)
+}
